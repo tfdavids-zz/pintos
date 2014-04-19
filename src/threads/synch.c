f77c7b32d8b6c4c1fc1bc7cd55e658079efeec33
Thomas Davids <tfdavids@gmail.com>
2014-04-19 00:14:08 -0700

    Removed an unnecessary line from before

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -362,7 +362,6 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  waiter.thread = thread_current ();
   list_push_back (&cond->waiters, &waiter.elem);
   lock_release (lock);
   sema_down (&waiter.semaphore);
