af490c1a9b12cfdcf5e153901d9aca5d1dfa451c
Robert Gasparyan <robertga@stanford.edu>
2014-05-16 11:17:51 -0700

    Cleaned up swap.[ch]

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -7,33 +7,37 @@
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
 
-static struct block *swap;
+static struct block *swap_device;
 static struct bitmap *swap_slots;
-static size_t sectors_per_page = BLOCK_SECTOR_SIZE / PGSIZE;
+static size_t sectors_per_page = PGSIZE / BLOCK_SECTOR_SIZE;
 size_t num_swap_slots;
 
 void swap_init (void)
 {
-  swap = block_get_role (BLOCK_SWAP);
-  num_swap_slots = block_size (swap) * sectors_per_page;
+  swap_device = block_get_role (BLOCK_SWAP);
+  if (swap_device == NULL)
+    PANIC ("No swap device found, can't initialize swap");
+
+  num_swap_slots = block_size (swap_device) / sectors_per_page;
   swap_slots = bitmap_create (num_swap_slots);
+  if (swap_slots == NULL)
+    PANIC ("bitmap creation failed--swap device is too large");
+
   bitmap_set_all (swap_slots, false);
 }
 
 
 
-size_t swap_writes_page (void *upage)
+size_t swap_write_page (void *upage)
 {
   size_t free_slot_index = bitmap_scan (swap_slots, 0, 1, false);
   if (free_slot_index == BITMAP_ERROR)
-    {
-      /* TODO: decide what to do when swap is full */
-    }
+    PANIC ("swap is full");
 
-  size_t i = 0;
-  for (; i < sectors_per_page; i++)
+  size_t i;
+  for (i = 0; i < sectors_per_page; i++)
     {
-      block_write (swap,
+      block_write (swap_device,
                    free_slot_index * sectors_per_page + i,
                    upage + i * BLOCK_SECTOR_SIZE);
 
@@ -51,10 +55,10 @@ bool swap_load_page (size_t slot_index, void *upage)
     {
       return false;
     }
-  size_t i = 0;
-  for (; i < sectors_per_page; i++)
+  size_t i;
+  for (i = 0; i < sectors_per_page; i++)
     {
-      block_read (swap,
+      block_read (swap_device,
                   slot_index * sectors_per_page + i,
                   upage + i * BLOCK_SECTOR_SIZE);
     }
