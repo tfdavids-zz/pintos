92e3757ce2877ba6f0edb5396e852a3d51c6842c
Thomas Davids <tfdavids@gmail.com>
2014-05-30 15:14:23 -0700

    Updated run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,4 @@
-cd src/userprog/build
-if [[ $1 =~ ^[0-9]+$ ]]; then
-    make clean
-    test=`ls ../../tests/userprog/*.ck | cut -c 7- | cut -f1 -d "." | uniq | sed -n "${1}p"`
-    make $test.result VERBOSE=1
-elif [ "$1" != "" ]; then
-    make clean
-    make tests/userprog/$1.result VERBOSE=1
-else
-    make clean
-    make check
-fi
+cd src/filesys/build
+make clean
+make check
 cd ../../..
