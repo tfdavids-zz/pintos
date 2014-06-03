398f8b96c9b0b00980908ec70baac01c8a8188b4
Akshay Kumar Agrawal <akshayka@stanford.edu>
2014-06-03 00:50:10 -0700

    Code clean-up and write-eviction revision Still racy and deadlock-y

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -5,7 +5,8 @@
 
 void cache_init (void);
 void cache_read (struct block *block, block_sector_t sector, void *buffer);
-void cache_write (struct block *block, block_sector_t sector, const void *buffer);
+void cache_write (struct block *block, block_sector_t sector,
+  const void *buffer);
 // void cache_read_bytes ()
 // void cache_write_bytes
 void cache_flush (void);
